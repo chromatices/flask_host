3c0b7d0a3adc584a33c13286020e314aee190f81
chromatices <pjh403@naver.com>
2022-09-30 16:20:58 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # rest_host
 rest host for test
+
+### Usage
+- shell(local, recommanded)
+```shell
+python main.py
+```
+
+- docker
+```docker
+docker build . -t [tagname:version]
+docker run --rm -it [tagname:version] /bin/bash
+```
---
+++ RESTHOST/src/rest-host.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, jsonify
+import os
+import json
+
+app = Flask(__name__)
+
+# for test env on docker
+# url = os.environ['REST_URL']
+
+# for test env on local
+url  = '0.0.0.0'
+
+@app.route("/test" ,methods=['POST'])
+def test():
+    # receive POST data
+    params = request.get_data()
+    received_params = json.loads(params.decode('utf-8'))
+
+    # error occur if json has different keys
+    if len(list(received_params.keys())) != 3:
+        raise ValueError("Answer key does not match!!")
+    else:
+        response = {
+            "result": "ok"
+        }
+
+    return jsonify(response)
+
+if __name__ == "__main__":
+    app.run(debug=True, host=url)
